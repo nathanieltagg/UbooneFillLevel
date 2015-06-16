2f479573408f75ceef1692c7883e7ae45578765d
Nathaniel Tagg <tagg@fnal.gov>
2015-06-16 17:39:52 -0500

    make it work

--- get_vals.sh
+++ get_vals.sh
@@ -2,10 +2,10 @@
 echo -e "Content-type: text/html\r\n"
 
 theVars="uB_Cryo_IFIX_1_0/TE190 uB_Cryo_IFIX_1_0/TE194 uB_Cryo_IFIX_1_0/LT124 uB_Cryo_IFIX_1_0/LT122"
-if [ -f /home/uboonesmc/setup_SMC_EPICS.sh ]; then
+if [ -f "/home/uboonesmc/setup_SMC_EPICS.sh" ]; then
   source /home/uboonesmc/setup_SMC_EPICS.sh
-  value=`caget -a -f1 $theVars`
-  echo $value
+  caget -a -f1 $theVars
+
 else
   echo "uB_Cryo_IFIX_1_0/TE190         2015-06-16 16:52:04.229338 100.00"
   echo "uB_Cryo_IFIX_1_0/TE194         2015-06-16 16:52:04.310855 106.40"
